07d1edbf143d89f72d4c8e9322b3fd6f864cb47f
DoctorHacks <brandonrikeler@icloud.com>
2023-11-09 21:26:06 -0500

    Feat: recursive division generation algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 /**
- * A module containing a Maze struct, capable of representing a two-dimensional labyrinth with
- * exactly one path from the entry point at the top-left to the goal point at the bottom-right.
+ * A module containing a Maze struct, capable of generating, representing, and solving a
+ * two-dimensional labyrinth with exactly one path from the entry point at the top-left to the goal
+ * point at the bottom-right.
  *
  * Authors: Brandon Ikeler, Travis Hahn
  */
@@ -9,7 +10,7 @@ extern crate rand;
 
 mod maze_operations {
     use colored::*;
-    use rand::{seq::SliceRandom, thread_rng};
+    use rand::{seq::SliceRandom, thread_rng, Rng};
     use std::fmt;
 
     #[derive(Debug)]
@@ -45,25 +46,34 @@ mod maze_operations {
     }
 
     impl Maze {
-        pub fn new(height: usize, width: usize, algorithm: CreationAlgorithm) -> Self {
+        pub fn new(dimensions: (usize, usize), algorithm: CreationAlgorithm) -> Self {
             use CreationAlgorithm::*;
-            if height < 2 || width < 2 {
+            // Mazes smaller than 3x3 don't make sense (dimensions.0/1 == 2 case handled below)
+            if dimensions.0 < 2 || dimensions.1 < 2 {
                 panic!("Can't create a maze this small")
             }
             // Mazes only work well with odd-number dimensions
-            // Example of name shadowing--you can redeclare variables in the same scope
-            // Also an example of how type names are optional if the compiler can infer them
-            let height = if height % 2 == 0 { height + 1 } else { height };
-            let width = if width % 2 == 0 { width + 1 } else { width };
+            let height = if dimensions.0 % 2 == 0 {
+                dimensions.0 + 1
+            } else {
+                dimensions.0
+            };
+            let width = if dimensions.1 % 2 == 0 {
+                dimensions.1 + 1
+            } else {
+                dimensions.1
+            };
 
             let cells: Vec<Vec<Cell>> = vec![
                 vec![
                     Cell {
                         // match statements must enumerate all variants in their arms
                         wall: match algorithm {
-                            RandomWalk => true,
-                            Debug => true,
-                            _ => false,
+                            // Recursive division starts with a grid of paths and constricts the
+                            // movable spaces with new walls
+                            RecursiveDivision => false,
+                            // All other algorithms start with a grid of walls and carve out a path
+                            _ => true,
                         },
                         visited: false
                     };
@@ -77,14 +87,6 @@ mod maze_operations {
                 RecursiveDivision => Self::gen_from_divide(cells),
                 Debug => Self::debug(cells),
             }
-        }
-
-        fn shuffle_directions() -> [Direction; 4] {
-            use Direction::*;
-            let mut directions = [North, South, East, West];
-            let mut rng = thread_rng();
-            directions.shuffle(&mut rng);
-            directions
         }
 
         fn gen_from_walk(mut cells: Vec<Vec<Cell>>, dimensions: (usize, usize)) -> Self {
@@ -153,7 +155,101 @@ mod maze_operations {
         }
 
         fn gen_from_divide(mut cells: Vec<Vec<Cell>>) -> Self {
-            unimplemented!();
+            // these are declared up here, because if their values were copied...
+            let height = cells.len();
+            let width = cells[0].len();
+
+            // walls on top and bottom
+            for i in 0..cells[0].len() {
+                cells[0][i].wall = true;
+                // here...
+                cells[height - 1][i].wall = true;
+            }
+
+            // walls on left and right
+            for row in &mut cells {
+                row[0].wall = true;
+                // or here,
+                row[width - 1].wall = true;
+                // it would cause a compilation error, because the loops borrow cells (or cells[0])
+                // mutably, and cells.len() (or cells[0].len()) borrows cells immutably, and we
+                // can't have a mutable and an immutable reference to cells (or cells[0]) active at
+                // the same time
+            }
+
+            Self::divide(&mut cells, (0, 0), (height - 1, width - 1));
+
+            Maze {
+                dimensions: (height, width),
+                goalpoint: (height - 2, width - 1),
+                entrypoint: (1, 0),
+                cells,
+            }
+        }
+
+        fn divide(
+            cells: &mut Vec<Vec<Cell>>,
+            top_left: (usize, usize),
+            bottom_right: (usize, usize),
+        ) {
+            let height = bottom_right.0 - top_left.0;
+            let width = bottom_right.1 - top_left.1;
+
+            // we can't divide further if the area to divide is already 1 cell narrow
+            if height == 2 || width == 2 {
+                return;
+            }
+
+            let mut rng = rand::thread_rng();
+            // if the area to divide is at least as tall as it is wide, draw a new horizontal wall
+            // on a random even index that divides the area, and punch a hole in it at a random odd
+            // index to retain the maze's connectedness
+            if height >= width {
+                // ranges exclude the max value
+                let mut wall_index = rng.gen_range((top_left.0 + 1)..bottom_right.0);
+                while wall_index % 2 != 0 {
+                    wall_index = rng.gen_range((top_left.0 + 1)..bottom_right.0);
+                }
+
+                for i in (top_left.1 + 1)..bottom_right.1 {
+                    cells[wall_index][i].wall = true;
+                }
+
+                let mut hole_index = rng.gen_range((top_left.1 + 1)..bottom_right.1);
+                while hole_index % 2 != 1 {
+                    hole_index = rng.gen_range((top_left.1 + 1)..bottom_right.1);
+                }
+
+                cells[wall_index][hole_index].wall = false;
+                Self::divide(cells, top_left, (wall_index, bottom_right.1));
+                Self::divide(cells, (wall_index, top_left.1), bottom_right);
+            } else {
+                let mut wall_index = rng.gen_range((top_left.1 + 1)..bottom_right.1);
+                while wall_index % 2 != 0 {
+                    wall_index = rng.gen_range((top_left.1 + 1)..bottom_right.1);
+                }
+
+                for i in (top_left.0 + 1)..bottom_right.0 {
+                    cells[i][wall_index].wall = true;
+                }
+
+                let mut hole_index = rng.gen_range((top_left.0 + 1)..bottom_right.0);
+                while hole_index % 2 != 1 {
+                    hole_index = rng.gen_range((top_left.0 + 1)..bottom_right.0);
+                }
+
+                cells[hole_index][wall_index].wall = false;
+                Self::divide(cells, top_left, (bottom_right.0, wall_index));
+                Self::divide(cells, (top_left.0, wall_index), bottom_right);
+            }
+        }
+
+        fn shuffle_directions() -> [Direction; 4] {
+            use Direction::*;
+            let mut directions = [North, South, East, West];
+            let mut rng = thread_rng();
+            directions.shuffle(&mut rng);
+            directions
         }
 
         fn debug(cells: Vec<Vec<Cell>>) -> Self {
@@ -161,15 +257,13 @@ mod maze_operations {
                 dimensions: (cells.len(), cells[0].len()),
                 goalpoint: (cells.len(), cells[0].len() - 1),
                 entrypoint: (0, 0),
-                cells, // if you specify cells before dimensions or goal, it
-                       // will not work, because the Maze struct takes ownership
-                       // of cells, which means cells goes out of scope in
-                       // debug()
-                       // This is field init shorthand syntax for: cells: cells,
-                       // by the way--when you specify a variable for
-                       // initializing a struct, if the variable is the same
-                       // name as the struct attribute, you only have to specify
-                       // the name of the local variable/struct attribute once
+                cells, // if you specify cells before dimensions or goal, it will not work, because
+                       // because the Maze struct takes ownership of cells, which means cells goes
+                       // out of scope in debug()
+                       // This is field init shorthand syntax for: cells: cells,by the way--when you
+                       // specify a variable for initializing a struct, if the variable is the same
+                       // name as the struct attribute, you only have to specify the name of the
+                       // local variable/struct attribute once
             }
         }
     }
@@ -179,6 +273,9 @@ mod maze_operations {
             for (y, row) in self.cells.iter().enumerate() {
                 for (x, cell) in row.iter().enumerate() {
                     write!(f, "{}", {
+                        // No idea why, but Unicode 00A0 (NO-BREAK SPACE) was the only Unicode
+                        // character I could find that would behave predictably when resizing the
+                        // terminal window
                         if (y, x) == self.entrypoint {
                             "\u{00A0}\u{00A0}".on_red()
                         } else if (y, x) == self.goalpoint {
@@ -192,7 +289,7 @@ mod maze_operations {
                         }
                     })?;
                 }
-                write!(f, "\n")?;
+                write!(f, "{}", "\u{00A0}\n".clear())?;
             }
             Ok(())
         }
@@ -201,6 +298,6 @@ mod maze_operations {
 
 use maze_operations::*;
 fn main() {
-    let maze: Maze = Maze::new(21, 21, CreationAlgorithm::RandomWalk);
+    let maze: Maze = Maze::new((25, 35), CreationAlgorithm::RecursiveDivision);
     println!("{}", maze);
 }
