cb7c4f50d5fc4c71a5314c080dcf4899d8fd9d04
Travis Hahn <soccerboy.1018@yahoo.com>
2023-11-12 15:57:09 -0500

    Bugfixes to main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use std::io;
 
 use crate::maze_operations::*;
 fn main() {
-    let mut rows, cols;
+    let mut rows;
+    let mut cols;
     let mut maze;
     let mut input = String::new();
     loop {
@@ -18,12 +19,12 @@ fn main() {
                 let mut input = String::new();
                 io::stdin().read_line(&mut input).expect("No line given.");
                 let mut nums = input.trim().split_whitespace();
-                rows: usize = nums
+                rows = nums
                     .next()
                     .expect("No Next.")
                     .parse()
                     .expect("Not a Valid Integer.");
-                cols: usize = nums
+                cols = nums
                     .next()
                     .expect("No Next")
                     .parse()
@@ -31,7 +32,7 @@ fn main() {
 
                 if cols >= 3 && rows >= 3{
                     break;
-                } else println("Rows and cols must be 3 or greater.");
+                } else{println!("Rows and cols must be 3 or greater.");}
             }
             //Loop for user to input how they want their maze to be generated
             //1:Prim Algorithm 2:Random Walk Algorithm 3:Recursive Division Algorithm
@@ -46,15 +47,15 @@ fn main() {
                     let x: usize = input.trim().parse().expect("Please input an integer.");
                     match x{
                         1 => {
-                            maze: Maze = Maze::new_from((rows,cols), CreationAlgorithm::Prim);
+                            maze = Maze::new_from((rows,cols), CreationAlgorithm::Prim);
                             break;
                         }
                         2 => {
-                            maze: Maze = Maze::new_from((rows,cols), CreationAlgorithm::RandomWalk);
+                            maze = Maze::new_from((rows,cols), CreationAlgorithm::RandomWalk);
                             break;
                         }
                         3 => {
-                            maze: Maze = Maze::new_from((rows,cols), CreationAlgorithm::RecursiveDivision);
+                            maze = Maze::new_from((rows,cols), CreationAlgorithm::RecursiveDivision);
                             break;
                         }
                         _ => println!("Please input an acceptable integer.")
@@ -71,14 +72,14 @@ fn main() {
                     let x: usize = input.trim().parse().expect("Please input an integer.");
                     match x{
                         1 => {
-                            maze.solve_from(self,SolvingAlgorithm::RecursiveBacktracking);
+                            maze.solve_from(SolvingAlgorithm::RecursiveBacktracking);
                             break;
                         }
                         2 => {
-                            maze.solve_from(self,SolvingAlgorithm::DeadEndFilling);
+                            maze.solve_from(SolvingAlgorithm::DeadEndFilling);
                             break;
                         }
-                        _ => println!("Please input an acceptable integer.");
+                        _ => {println!("Please input an acceptable integer.");}
                     }
                 }
                 println!("{}", maze);
