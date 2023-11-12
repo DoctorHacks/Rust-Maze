a976d91b0bc789ac0dcc30b3110db7833a3937dc
Travis Hahn <soccerboy.1018@yahoo.com>
2023-11-12 14:40:19 -0500

    Modifying Main

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ use maze::maze_operations;
 use std::io;
 
 use crate::maze_operations::*;
-
 fn main() {
     let mut rows: usize = 0;
     let mut cols: usize = 0;
+    let mut input = String::new();
     loop {
-        println!("Type 1 to Create and Solve a Maze.\nType 2 to Quit");
+        println!("Type 1 to Create and Solve a Maze.\n Type 2 to Quit");
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("No Line Given");
         let x: usize = input.trim().parse().expect("Please Input an Integer");
         if x == 1 {
+            println!("Give the Dimensions for the Maze in format: rows cols");
             loop {
-                println!("Give the Dimensions for the Maze in format: rows cols");
                 let mut input = String::new();
                 io::stdin().read_line(&mut input).expect("No Line Given");
                 let mut nums = input.trim().split_whitespace();
